5e066966fb16587212d82c7dedaf333d19380e2c
ankit <ankitsingh435517@gmail.com>
2021-05-22 20:07:13 +0530

    make home page

--- server.js
+++ server.js
@@ -18,15 +18,26 @@ const expressLayouts = require('express-ejs-layouts')
 const path = require('path')
 
 
+//Assets
+app.use(express.static('public'))
 
-app.get('/',(req,res)=>{ //it takes info from below set-up of app.use,set..
-    res.render('home')
-})
 
 //set template engine
 app.use(expressLayouts)
 app.set('views',path.join(__dirname,'/resources/views'))
 app.set('view engine','ejs')
+
+
+
+app.get('/',(req,res)=>{ //it takes info from below set-up of app.use,set..
+    res.render('home')
+})
+
+app.get('/cart',(req,res)=>{
+    res.render('customers/cart')
+})
+
+
 
 app.listen(PORT,()=>{
     console.log(`Listening on port ${PORT}`)
